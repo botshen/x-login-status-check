85d60e2c4c0cd3a6505e5a7979ef349b31aaecde
Xin shen <363088847@qq.com>
2025-09-30 14:51:27 +0800

    feat: 更新项目名称和描述，添加登录状态检查功能 - 修改了 package.json 中的名称和描述 - 在 wxt.config.ts 中添加了 webExt 配置和权限 - 在 HelloWorld.vue 中实现了检查登录状态的功能 - 清理了 App.vue 中的多余代码

--- components/HelloWorld.vue
+++ components/HelloWorld.vue
@@ -5,26 +5,15 @@ defineProps({
   msg: String,
 });
 
-const count = ref(0);
+const checkLogin = () => {
+  browser.cookies.get({ url: 'https://x.com', name: 'auth_token' }, (cookie) => {
+    console.log(cookie);
+  });
+};
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the WXT and Vue logos to learn more</p>
+ <button @click="checkLogin">检查登录状态</button>
 </template>
 
 <style scoped>
--- wxt.config.ts
+++ wxt.config.ts
@@ -3,4 +3,12 @@ import { defineConfig } from 'wxt';
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ['@wxt-dev/module-vue'],
+  webExt: {
+    disabled: true,
+  },
+  manifest: {
+    permissions: ['storage', 'tabs', 'cookies'],
+    host_permissions: ['https://x.com/*'],
+    
+  },
 });
